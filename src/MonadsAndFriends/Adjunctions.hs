46d819708be0fe482e0030431997db2859c59c33
PureFunctor <purefunctor@gmail.com>
2021-01-08 18:53:41 +0800

    Add intuition for adjunctions of reader functions

--- src/MonadsAndFriends/Adjunctions.hs
+++ src/MonadsAndFriends/Adjunctions.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module MonadsAndFriends.Adjunctions where
 
 
@@ -65,4 +67,69 @@ instance Adjunction ((,) a) ((->) a) where
 
 -- Why do these relationships matter to us? Interestingly
 -- enough, these adjunctions form the relationships between
--- monads and comonads. More on that on this section. TODO.
+-- monads and comonads. More on that on this section.
+
+-- The `Reader` monad helps us abstract over functions that
+-- take some read-only "environment" and produce some overall
+-- result.
+newtype Reader' e r = Reader { runReader :: e -> r }
+  deriving (Functor, Applicative, Monad)
+
+-- Given this, we can define functions to produce `Reader`s,
+-- allowing us to add more information. The most general form
+-- would be `a -> Reader e r`.
+extra :: Int -> Reader' [Int] Int
+extra x = Reader \xs -> sum $ x : xs
+
+fourtyTwo :: Int
+fourtyTwo = runReader (extra 2) [20, 20]
+
+-- As an exercise, we can "unwrap" the inner function type from
+-- the `Reader` monad and use that instead. This gives us the
+-- general form of `a -> e -> r`.
+extra' :: Int -> [Int] -> Int
+extra' x xs = sum $ x : xs
+
+fourtyTwo' :: Int
+fourtyTwo' = extra' 2 [20, 20]
+
+-- As another exercise, we then uncurry the `extra'` function.
+-- This would then give us the general form of `(a, e) -> r`.
+extra'' :: (Int, [Int]) -> Int
+extra'' = uncurry extra'
+
+-- With this in mind, let's look at these two forms:
+--
+-- a -> e -> r
+-- (a, e) -> r
+--
+-- We learned earlier that we can form an adjunction between
+-- the `(->) a` and `(,) a` functors, but before we proceed,
+-- we have to adjust something first:
+--
+-- `(a, e) -> r` into `(e, a) -> r`
+--
+-- Remember that the adjunction between `(->) a` and `(,) a`
+-- isn't just `curry`-ing and `uncurry`-ing functions, but
+-- rather, it also involves flipping the arguments.
+--
+-- This gives us our new pair of functions:
+--
+-- a -> e -> r
+-- (e, a) -> r
+--
+-- When generalized:
+--
+-- a -> f r
+-- g a -> r
+--
+-- We lose some type information by replacing `(->) e` and `(,) e`
+-- with `f` and `g` respectively, but we can assume that `f` and
+-- `g` both consumes `e`. This will be useful in the next section.
+--
+-- That signature looks just like an adjunction!
+--
+-- One question that can be asked is: if we found the adjunction
+-- for `a -> e -> r`, which is abstracted into `a -> Reader e r`,
+-- could we define such a data type that abstracts over the
+-- adjunction of the former? TODO.
